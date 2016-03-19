c6aa010c2b6badcca3cc4dacea073881bb23c708
Jeff Ratliff <jefrat@gmail.com>
2016-03-19 16:32:06 -0400

    exercise 4

---
+++ ex4.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# ex4.rb	3/19/2016
+# From chapter 4 of "Learn Ruby the Hard Way"
+#
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
