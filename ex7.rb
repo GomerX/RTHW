2d405b1990a5d2a904fd6f06ff7f69e8e9e85893
Jeff Ratliff <jefrat@gmail.com>
2016-03-22 22:59:00 -0400

    Adding comments. For translating practice.

--- ex7.rb
+++ ex7.rb
@@ -1,14 +1,20 @@
 #!/usr/bin/env ruby
 # ex7.rb	3/22/2016
 # From chapter 7 of "Learn Ruby the Hard Way"
-# More stuff to print
+# More stuff to print and puts
 
+# It is a rhyme
 puts "Mary had a little lamb."
+# interpreting the string has no purpose. It's not a variable.
 puts "Its fleece was white as #{'snow'}."
+# More of the rhyme..
 puts "And everywhere that Mary went."
+# Making a line of dots. Maybe a divider?
 puts "." * 10  # what'd that do?
 
+# setting a bunch of variables one by one
 end1 = "C"
+# Because we don't know about arrays yet.
 end2 = "h"
 end3 = "e"
 end4 = "e"
@@ -23,4 +29,5 @@ end12 = "r"
 
 # watch that print vs. puts on this line what's it do?
 print end1 + end2 + end3 + end4 + end5 + end6
+# apparently print doesn't give the trailing newline
 puts end7 + end8 + end9 + end10 + end11 + end12
