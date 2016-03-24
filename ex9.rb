9e65a14cde081763811b12abe82ed83e5d06691c
Jeff Ratliff <jefrat@gmail.com>
2016-03-23 21:57:26 -0400

    Comments for ex9.

--- ex9.rb
+++ ex9.rb
@@ -4,12 +4,17 @@
 
 # Here's some new strange stuff, remeber type it exactly. 
 
+# All the days separated by spaces
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# All the months separated by newlines
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# show the days and months formatted differently
 puts "Here are the days: #{days}"
 puts "Here are the months: #{months}"
 
+# Triple double quotes means take everything literally til we find more
+# triple double quotes.
 puts """
 There's something going on here.
 With the three double-quotes.
