d6ae209d0b36f323ec0ad9f671896928204b6097
Jeff Ratliff <jefrat@gmail.com>
2016-03-24 23:45:16 -0400

    Exercise 11. Does not work on the Windows system.

---
+++ ex11.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# ex11.rb	3/24/2016
+# From chapter 11 of "Learn Ruby the Hard Way"
+
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
