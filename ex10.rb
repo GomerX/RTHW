765a5d4a7c83829f07a8d5a67311922ddcb14fb6
Jeff Ratliff <jefrat@gmail.com>
2016-03-24 22:57:17 -0400

    Started exercise 10

---
+++ ex10.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# ex10.rb	3/24/2016
+# From chapter 10 of "Learn Ruby the Hard Way"
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
