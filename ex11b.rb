d9b691f9a334e485e6d96e0019d6d7b5ecbfaced
Jeff Ratliff <jeff.ratliff@lendkey.com>
2016-03-25 22:15:02 -0400

    My own bizzaro version of ex11.

---
+++ ex11b.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# ex11.rb	3/24/2016
+# From chapter 11 of "Learn Ruby the Hard Way"
+
+puts "What are puppies? "
+pup = gets.chomp
+puts "Where are the dog? "
+dog = gets.chomp
+puts "Is it necessarilly necessary to eat a squid?"
+squid = gets.chomp
+puts " What do you think about Rome? "
+rome = gets.chomp
+
+puts "Well, puppies are #{pup} and we all know that dogs are at #{dog}."
+puts "Must we eat squid? #{squid}. And Rome is like this \"#{rome}\"."
