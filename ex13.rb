cfb297c451d64654e80cd799b7c4703057ba6968
Jeff Ratliff <jefrat@gmail.com>
2016-03-27 22:43:48 -0400

    Exercise for chapter 13.

---
+++ ex13.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# ex13.rb           3/27/2016
+# The code from chapter 13
+
+first, second, third = ARGV
+
+puts "Your first variable is #{first}."
+puts "Your second variable is #{second}."
+puts "Your third variable is #{third}."
