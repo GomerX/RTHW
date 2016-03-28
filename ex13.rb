f6284d40ab85589e0e396b5d66ec9e537e25d6bc
Jeff Ratliff <jeff.ratliff@lendkey.com>
2016-03-27 22:52:52 -0400

    This is more like the book.

--- ex13.rb
+++ ex13.rb
@@ -4,6 +4,6 @@
 
 first, second, third = ARGV
 
-puts "Your first variable is #{first}."
-puts "Your second variable is #{second}."
-puts "Your third variable is #{third}."
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
